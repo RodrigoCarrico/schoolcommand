2b583948eb7aff4cb6b091d67ec460f5fed6b328
Carrico <rodrigo.carrico@totvs.com.br>
2020-09-08 15:45:05 +0100

    incluindo Swagger

--- src/main/java/br/com/onboard/schoolcommand/SchoolcommandApplication.java
+++ src/main/java/br/com/onboard/schoolcommand/SchoolcommandApplication.java
@@ -3,9 +3,11 @@ package br.com.onboard.schoolcommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableSwagger2
 public class SchoolcommandApplication {
 
     public static void main(String[] args) {
